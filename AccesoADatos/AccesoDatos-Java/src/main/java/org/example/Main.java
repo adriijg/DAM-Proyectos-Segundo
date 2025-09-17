98f0bd05faa1727f494169d7b5de749b2d1f947c
adriijg <adrianjunquera@hotmail.es>
2025-09-17 16:49:14 +0200

    Nuevos cambios 17/09

--- AccesoADatos/AccesoDatos-Java/src/main/java/org/example/Main.java
+++ AccesoADatos/AccesoDatos-Java/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ficheros();
+    }
+
+    private static void ficheros() {
+//        File f = new File ("../src/main/java/org/example/Fichero.txt");
+        File f = new File("files/Fichero.txt");
+        f.getParentFile().mkdirs(); // crea la carpeta "files" si no existe
+
+        try (FileWriter fw = new FileWriter(f)) {
+            fw.write("Hola mundo");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
